7a4a3fdd3c8f72ef4eefeca30310a30353ba3e4f
Florian Krohmer <florian.krohmer@gmail.com>
2019-03-19 10:46:32 +0100

    include filepicker

--- src/Resources/contao/dca/tl_module.php
+++ src/Resources/contao/dca/tl_module.php
@@ -1,4 +1,13 @@
 <?php
 
 // Add palette to tl_module
-$GLOBALS['TL_DCA']['tl_module']['palettes']['helloWorld'] = '{title_legend},name,headline,type;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID,space';
+$GLOBALS['TL_DCA']['tl_module']['palettes']['helloWorld'] = '{title_legend},name,headline,type;{config_legend}hello_file;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID,space';
+
+$GLOBALS['TL_DCA']['tl_module']['fields']['hello_file'] = array
+(
+    'label'               => 'File',
+    'exclude'             => true,
+    'inputType'           => 'fileTree',
+	'eval'                => array('fieldType'=>'radio', 'files'=>true, 'filesOnly'=>true, 'tl_class'=>'clr', 'extensions'=>$GLOBALS['TL_CONFIG']['validImageTypes']),
+    'sql'                 => "blob NULL"
+);
